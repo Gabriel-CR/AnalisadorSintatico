3c2a908f754a867059104598d2c2c6ccfcd09af1
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-05-21 17:28:26 -0300

    documentando Gramatica

--- versao_cpp/SINTATICO/headers/gramatica.h
+++ versao_cpp/SINTATICO/headers/gramatica.h
@@ -18,11 +18,33 @@ class Gramatica
     set<string> nao_terminais;
 
 public:
+    /*
+    * Construtor da classe Gramatica
+    @param caminho: caminho do arquivo de gramatica
+    */
     Gramatica(string caminho);
+    /*
+    Ler arquivo com a gramatica usando o caminho
+    passado no construtor
+    */
     void read_gramatica_file();
-    void make_terminais();
+    /*
+    * Deve ser executado apos a leitura do arquivo de gramatica
+    e cria o conjunto de nao terminais na gramatica
+    * Usa como principio que os não terminais são aqueles que
+    estão antes de -> na gramatica
+    */
     void make_nao_terminais();
+    /*
+    * Deve ser executado apos a leitura do arquivo de gramatica
+    e cria o conjunto de nao terminais na gramatica
+    * Cria o conjunto de terminais na gramatica
+    * Usa como principio que os não terminais são aqueles que
+    não estão na lista de terminais
+    */
+    void make_terminais();
 
+    // GETS
     map<string, vector<string>> get_gramatica();
     set<string> get_terminais();
     set<string> get_nao_terminais();
